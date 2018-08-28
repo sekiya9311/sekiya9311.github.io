e6dd16fc713296212fc120e104197cf4dbfb6a91
sekiya9311 <sekiya9311@gmail.com>
2018-08-29 01:05:30 +0900

    制作物のっけるページ用コンポーネント追加

---
+++ src/components/MyAppsPage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export class MyAppsPage extends React.Component<{}, {}> {
+  constructor() {
+    super({});
+  }
+  render() {
+    return (
+      <div>
+        MyAppsPage.tsx
+      </div>
+    );
+  }
+}
